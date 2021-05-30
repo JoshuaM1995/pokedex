73a8dba989b3057ffffb9452a040f3424e88cddc
joshuam1995 <joshuamcnabb1995@gmail.com>
2021-05-30 14:59:39 -0600

    eslint --fix

--- app/screens/welcome/welcome-screen.tsx
+++ app/screens/welcome/welcome-screen.tsx
@@ -1,85 +1,90 @@
-import React from "react"
-import { View, ViewStyle, TextStyle, ImageStyle, SafeAreaView } from "react-native"
-import { useNavigation } from "@react-navigation/native"
-import { observer } from "mobx-react-lite"
-import { Button, Header, Screen, Text, Wallpaper, AutoImage as Image } from "../../components"
-import { color, spacing, typography } from "../../theme"
-const bowserLogo = require("./bowser.png")
+import React from 'react';
+import {
+  View, ViewStyle, TextStyle, ImageStyle, SafeAreaView,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { observer } from 'mobx-react-lite';
+import {
+  Button, Header, Screen, Text, Wallpaper, AutoImage as Image,
+} from '../../components';
+import { color, spacing, typography } from '../../theme';
 
-const FULL: ViewStyle = { flex: 1 }
+const bowserLogo = require('./bowser.png');
+
+const FULL: ViewStyle = { flex: 1 };
 const CONTAINER: ViewStyle = {
   backgroundColor: color.transparent,
   paddingHorizontal: spacing[4],
-}
+};
 const TEXT: TextStyle = {
   color: color.palette.white,
   fontFamily: typography.primary,
-}
-const BOLD: TextStyle = { fontWeight: "bold" }
+};
+const BOLD: TextStyle = { fontWeight: 'bold' };
 const HEADER: TextStyle = {
   paddingTop: spacing[3],
   paddingBottom: spacing[4] + spacing[1],
   paddingHorizontal: 0,
-}
+};
 const HEADER_TITLE: TextStyle = {
   ...TEXT,
   ...BOLD,
   fontSize: 12,
   lineHeight: 15,
-  textAlign: "center",
+  textAlign: 'center',
   letterSpacing: 1.5,
-}
+};
 const TITLE_WRAPPER: TextStyle = {
   ...TEXT,
-  textAlign: "center",
-}
+  textAlign: 'center',
+};
 const TITLE: TextStyle = {
   ...TEXT,
   ...BOLD,
   fontSize: 28,
   lineHeight: 38,
-  textAlign: "center",
-}
+  textAlign: 'center',
+};
 const ALMOST: TextStyle = {
   ...TEXT,
   ...BOLD,
   fontSize: 26,
-  fontStyle: "italic",
-}
+  fontStyle: 'italic',
+};
 const BOWSER: ImageStyle = {
-  alignSelf: "center",
+  alignSelf: 'center',
   marginVertical: spacing[5],
-  maxWidth: "100%",
+  maxWidth: '100%',
   width: 343,
   height: 230,
-}
+};
 const CONTENT: TextStyle = {
   ...TEXT,
-  color: "#BAB6C8",
+  color: '#BAB6C8',
   fontSize: 15,
   lineHeight: 22,
   marginBottom: spacing[5],
-}
+};
 const CONTINUE: ViewStyle = {
   paddingVertical: spacing[4],
   paddingHorizontal: spacing[4],
   backgroundColor: color.palette.deepPurple,
-}
+};
 const CONTINUE_TEXT: TextStyle = {
   ...TEXT,
   ...BOLD,
   fontSize: 13,
   letterSpacing: 2,
-}
-const FOOTER: ViewStyle = { backgroundColor: "#20162D" }
+};
+const FOOTER: ViewStyle = { backgroundColor: '#20162D' };
 const FOOTER_CONTENT: ViewStyle = {
   paddingVertical: spacing[4],
   paddingHorizontal: spacing[4],
-}
+};
 
-export const WelcomeScreen = observer(function WelcomeScreen() {
-  const navigation = useNavigation()
-  const nextScreen = () => navigation.navigate("demo")
+export const WelcomeScreen = observer(() => {
+  const navigation = useNavigation();
+  const nextScreen = () => navigation.navigate('demo');
 
   return (
     <View testID="WelcomeScreen" style={FULL}>
@@ -114,5 +119,5 @@ export const WelcomeScreen = observer(function WelcomeScreen() {
         </View>
       </SafeAreaView>
     </View>
-  )
-})
+  );
+});
