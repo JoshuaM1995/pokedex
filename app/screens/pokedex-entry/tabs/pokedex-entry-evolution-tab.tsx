16c44ddd7d00e608b8e5d2649d701bdec981192a
joshuam1995 <joshuamcnabb1995@gmail.com>
2021-06-06 20:32:38 -0600

    feat: added evolution timeline page

--- app/screens/pokedex-entry/tabs/pokedex-entry-evolution-tab.tsx
+++ app/screens/pokedex-entry/tabs/pokedex-entry-evolution-tab.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import Timeline from 'react-native-timeline-flatlist';
+import { color } from '../../../theme/color';
 
 const PokedexEntryEvolutionTab = () => {
   return (
-    <div>
-      {'bulbasaur\'s evolutions'}
-    </div>
+    <Timeline
+      data={[
+        {
+          time: 'Level 16',
+          title: 'Ivysaur',
+          description: '#002',
+          icon: require('../../../../assets/images/pokemon/2.png'),
+        },
+        {
+          time: 'Level 16',
+          title: 'Venasaur',
+          description: '#003',
+          icon: require('../../../../assets/images/pokemon/3.png'),
+        },
+      ]}
+      innerCircle="icon"
+      listViewStyle={{ paddingTop: 40 }}
+      rowContainerStyle={{ minHeight: 100 }}
+      lineColor={color.palette.green}
+      circleColor="transparent"
+      eventDetailStyle={{ marginBottom: 75 }}
+      timeStyle={{ marginRight: 50 }}
+      iconStyle={{ width: 100, height: 100 }}
+      titleStyle={{ marginLeft: 50 }}
+      descriptionStyle={{ marginLeft: 50 }}
+    />
   );
 };
 
